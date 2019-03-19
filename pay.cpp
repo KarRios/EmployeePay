f0eff6d97e3cc97675e567f0ed7e8586ac5223b8
student <riosk@csu.fullerton.edu>
2019-03-19 16:46:50 -0700

    emplace_back() is used

--- pay.cpp
+++ pay.cpp
@@ -1,46 +1,38 @@
 #include "person.cpp"
 #include <iostream>
+#include <vector>
 #include <fstream>
 using namespace std;
 
-void readData(Person a[],int size);
-void writeData(Person a[],int size);
+void readData(vector<Person> a);
+void writeData(vector<Person> a);
 
 int main(){
-  Person employees[20];
+  vector<Person> employees;
 
-  readData(employees,20);
-  writeData(employees,20);
+  readData(employees);
+  writeData(employees);
 
   return 0;
 }
 
-void readData(Person a[],int size){
+void readData(vector<Person> a){
   int num = 0;
   string fname,lname,trash;
   float pay, hours;
   ifstream Input;
   Input.open("input.txt");
-  for(int i = 0; i < size; ++i){
-    Input >> fname >> lname >> pay >> hours;
+  while(!Input.eof()){
+    a.emplace_back(fname,lname,pay,hours);
     getline(Input,trash);
-    a[i].setFirstName(fname);
-    a[i].setLastName(lname);
-    a[i].setPayRate(pay);
-    a[i].setHoursWorked(hours);
   }
   Input.close();
 }
 
-void writeData(Person a[],int size){
+void writeData(vector<Person> a){
   ofstream output;
   output.open("output.txt");
-  output << a[0].fullName() << " " << a[0].totalPay() << "\n";
-  for(int i = 1; i < size; ++i){
-    if(a[i].fullName() == a[i-1].fullName()){
-      break;
-    }
-    
+  for(int i = 0;i < a.size(); ++i){
     output << a[i].fullName() << " " << a[i].totalPay() << "\n";
   }
 }
